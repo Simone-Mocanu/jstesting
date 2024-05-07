b160b2f4ad4920178a69b3006580945cfa069a49
beji <simone0443@outlook.com>
2024-05-07 06:26:10 +0300

    implemented push and pop functionality of the stack and tested

--- test/stack.test.js
+++ test/stack.test.js
@@ -3,18 +3,50 @@ class Stack {
         this.top = -1;
         this.items = {};
     }
+
+    get peek() {
+        return this.items[this.top];
+    }
+
+    push(value) {
+        this.top += 1;
+        this.items[this.top] = value;
+    }
+
+    pop() {
+        this.items[this.top] = undefined;
+        this.top -= 1;
+    }
 }
 
 describe("My stack", () => {
+    let stack;
+
+    beforeEach(() => {
+        stack = new Stack();
+    });
+    
     it('is created empty', () => {
-       const stack = new Stack();
 
        expect(stack.top).toBe(-1);
        expect(stack.items).toEqual({});
     });
 
 
-    it.todo('can push to the top');
+    it('can push to the top', () => {
+        stack.push('🥑');
+        expect(stack.top).toBe(0);
+        expect(stack.peek).toBe('🥑');
+    });
     
-    it.todo('can pop off');
+    it('can pop off', () => {
+        stack.push('🥑');
+        expect(stack.top).toBe(0);
+        stack.push('🥑');
+        expect(stack.top).toBe(1);
+        stack.pop();
+        expect(stack.top).toBe(0);
+        expect(stack.items).toEqual({"0": "🥑"});
+        expect(stack.peek).toBe("🥑");
+    });
 })
